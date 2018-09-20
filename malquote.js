9808d7245f83a28b8553b6b25178eb8d3290a433
SimpleBinary <40658215+SimpleBinary@users.noreply.github.com>
2018-09-20 19:46:47 +1000

    Create malquote.js

---
+++ malquote.js
@@ -0,0 +1,19 @@
+
+let Quotes = [
+"Life's too short to not own a dog", 
+"Yo", 
+"You are a really unlucky person if you aren't me", 
+"The Netflickses is a TV App", 
+"I am Professional Help", 
+"What do you want with me, Non- dog?", 
+"You are who you are, even if you aren't a dog", 
+"All these Humans thinking tey are so smart, while we do all the hard work" ,
+"Humans are so unhygenic, eating with those metal things",
+"How many times do I have to tell you, A credit card is something with money on it",
+"You're too lucky, you Humans",
+"Your species would've been wiped out thousands of times without us dogs",
+"Oh no, not a Cat"
+];
+let x = Math.floor(Math.random() * 14);
+let y = Qoutes[x]
+document.getElementById("Quote").innerHTML = y;
